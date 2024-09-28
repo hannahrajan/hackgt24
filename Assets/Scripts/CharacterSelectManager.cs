3fd978a5ac9264911274be68d41e8d55f9cc2b99
hannahrajan <66148049+hannahrajan@users.noreply.github.com>
2024-09-28 15:32:57 -0400

    help pt 3

---
+++ Assets/Scripts/CharacterSelectManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectManager : MonoBehaviour
+{
+
+    GameObject leftButton1;
+
+    GameObject[] leftButtons = new GameObject[5];
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftButtons[0] = leftButton1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Strength.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Strength
+{
+    public int id;
+    public string name;
+    public GameObject model;
+    public GameObject sprite;
+    public string dialogue;
+    public Dictionary<string, int> points;
+
+    public Strength(int id, string name, GameObject model, GameObject sprite, string dialogue)
+    {
+        this.id = id;
+        this.name = name;
+        this.model = model;
+        this.sprite = sprite;
+        this.dialogue = dialogue;
+        points = new Dictionary<string, int>
+        {
+            { "Time", 0 },
+            { "Likability", 0 },
+            { "Health / Wellbeing", 0},
+            { "Fulfillment / Esteem", 0 },
+            { "Creativity", 0 }
+        };
+    }
+
+    public void addPoints(int time, int like, int health, int fulfill, int create)
+    {
+        points["Time"] += time;
+        points["Likeability"] += like;
+        points["Health / Wellbeing"] += health;
+        points["Fulfillment / Esteem"] += fulfill;
+        points["Creativity"] += create;
+    }
+
+    public override string ToString()
+    {
+        return id + " {" +
+            name + ", Model: " +
+            model.name + ", Sprite: " +
+            sprite.name + ", ";
+    }
+}
